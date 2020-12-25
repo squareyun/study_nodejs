13e13f2f6c709315771981d0c9ff60fc0153b7ed
squareyun <squareyun@gmail.com>
2020-12-25 16:27:12 +0900

    Study undefined, not found

--- main.js
+++ main.js
@@ -5,37 +5,64 @@ var url = require("url");
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
 
-  if (_url == "/") {
-    title = "Welcome";
+  if (pathname === "/") {
+    if (title === undefined) {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        title = "Welcome";
+        description = "Hello, Node.js";
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200); // 서버가 브라우저에게 200을 주면 파일을 성공적으로 전송했다는 의미
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200); // 서버가 브라우저에게 200을 주면 파일을 성공적으로 전송했다는 의미
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404); // 404를 주면 파일을 찾을 수 없다는 의미
+    response.end("Not found");
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
-    response.end(template);
-  });
 });
 app.listen(3000);
