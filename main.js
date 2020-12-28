2d376439a263248eef1c81700a1c057d650c336b
squareyun <squareyun@gmail.com>
2020-12-28 21:24:32 +0900

    Study fs.unlink

--- main.js
+++ main.js
@@ -64,7 +64,12 @@ var app = http.createServer(function (request, response) {
               title,
               list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `<a href="/create">create</a>
+               <a href="/update?id=${title}">update</a>
+               <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+               </form>`
             );
             response.writeHead(200); // 서버가 브라우저에게 200을 주면 파일을 성공적으로 전송했다는 의미
             response.end(template);
@@ -150,6 +155,19 @@ var app = http.createServer(function (request, response) {
           response.writeHead(302, { Location: `/?id=${title}` }); // 리다이렉션
           response.end();
         });
+      });
+    });
+  } else if (pathname === "/delete_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body += data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (err) {
+        response.writeHead(302, { Location: `/` }); // 리다이렉션
+        response.end();
       });
     });
   } else {
