3aaa1917d340e01f0ae331ac58cfd5e5897d78af
squareyun <squareyun@gmail.com>
2021-08-21 16:14:56 +0900

    백엔드에서 로그인 기능 구현

--- woorimIT/app/src/routes/home/home.ctrl.js
+++ woorimIT/app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const user = {
+    id: ["square", "팀장", "개발"],
+    psword: ["1234", "1234", "12345"],
+};
+
 const output = {
     home: (req, res) => {
         res.render("home/index");
@@ -12,7 +17,22 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if(user.id.includes(id)) {
+            const idx = user.id.indexOf(id);
+            if (user.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
     },
 };
 
