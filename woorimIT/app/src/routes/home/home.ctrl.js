98f6e9e74e21300bed1b1d158fd04661f2c0ed81
squareyun <squareyun@gmail.com>
2021-08-23 15:59:05 +0900

    회원가입 페이지 구현

--- woorimIT/app/src/routes/home/home.ctrl.js
+++ woorimIT/app/src/routes/home/home.ctrl.js
@@ -6,10 +6,12 @@ const output = {
     home: (req, res) => {
         res.render("home/index");
     },
-
     login: (req, res) => {
         res.render("home/login");
     },
+    register: (req, res) => {
+        res.render("home/register");
+    }
 };
 
 const process = {
