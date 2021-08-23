d6285808bdebde28efe50702aad93575609a76ef
squareyun <squareyun@gmail.com>
2021-08-23 23:11:56 +0900

    데이터 파일로 관리하기 temp

--- woorimIT/app/src/models/UserStorage.js
+++ woorimIT/app/src/models/UserStorage.js
@@ -1,14 +1,10 @@
 "use strict";
 
-class UserStorage {
-    static #users = {
-        id: ["square", "팀장", "개발"],
-        psword: ["1234", "1234", "12345"],
-        name: ["네모", "팀장님", "개발자"],
-    };
+const fs = require("fs").promises;
 
+class UserStorage {
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -19,19 +15,24 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // [id, psword, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+        return fs.readFile("./src/databases/users.json")
+            .then((data) => {
+                if (err) throw err;
+                const users = JSON.parse(data);
+                const idx = users.id.indexOf(id);
+                const usersKeys = Object.keys(users); // [id, psword, name]
+                const userInfo = usersKeys.reduce((newUser, info) => {
+                    newUser[info] = users[info][idx];
+                    return newUser;
+                }, {});
+        
+                return userInfo;
+            })
+            .catch(console.error);
     }
 
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
