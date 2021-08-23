021db0dee42a7f0e8074287bb4aeb52787ff29cc
squareyun <squareyun@gmail.com>
2021-08-23 15:37:55 +0900

    User 모델 구현

--- woorimIT/app/src/models/UserStorage.js
+++ woorimIT/app/src/models/UserStorage.js
@@ -4,6 +4,7 @@ class UserStorage {
     static #users = {
         id: ["square", "팀장", "개발"],
         psword: ["1234", "1234", "12345"],
+        name: ["네모", "팀장님", "개발자"],
     };
 
     static getUsers(...fields) {
@@ -15,6 +16,18 @@ class UserStorage {
             return newUsers;
         }, {});
         return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); // [id, psword, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
     }
 }
 
