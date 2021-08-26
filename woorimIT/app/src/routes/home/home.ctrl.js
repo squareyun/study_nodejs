50f07f7d0fca69f8bbbd81cf96b149a3cdb07e9d
squareyun <squareyun@gmail.com>
2021-08-27 00:51:49 +0900

    winston 프로젝트에 적용

--- woorimIT/app/src/routes/home/home.ctrl.js
+++ woorimIT/app/src/routes/home/home.ctrl.js
@@ -1,33 +1,53 @@
 "use strict";
 
+const logger = require("../../config/logger");
 const User = require("../../models/User");
 
 const output = {
-    home: (req, res) => {
-        res.render("home/index");
-    },
-    login: (req, res) => {
-        res.render("home/login");
-    },
-    register: (req, res) => {
-        res.render("home/register");
-    }
+  home: (req, res) => {
+    logger.info(`GET / 200 "홈 화면으로 이동"`);
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    logger.info(`GET /login 200 "로그인 화면으로 이동"`);
+    res.render("home/login");
+  },
+  register: (req, res) => {
+    logger.info(`GET /register 200 "회원가입 화면으로 이동"`);
+    res.render("home/register");
+  },
 };
 
 const process = {
-    login: async (req, res) => {
-        const user = new User(req.body);
-        const response = await user.login();
-        return res.json(response);
-    },
-    register: async (req, res) => {
-        const user = new User(req.body);
-        const response = await user.register();
-        return res.json(response);
-    },
+  login: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.login();
+    if (response.err)
+      logger.error(
+        `POST /login 200 Response: "success: ${response.success}, ${response.err}`
+      );
+    else
+      logger.info(
+        `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}`
+      );
+    return res.json(response);
+  },
+  register: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.register();
+    if (response.err)
+      logger.error(
+        `POST /login 200 Response: "success: ${response.success}, ${response.err}`
+      );
+    else
+      logger.info(
+        `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}`
+      );
+    return res.json(response);
+  },
 };
 
 module.exports = {
-    output,
-    process,
+  output,
+  process,
 };
